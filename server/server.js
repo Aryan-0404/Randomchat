8ba992a2a3ba037bf9dba82ec3f1cf950b0621fd
Aryan-0404 <aryansinghhad@gmail.com>
2025-02-09 19:13:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
-    maxHttpBufferSize: 10e6 // 5 MB
-});
+const io = socketIo(server);
 
 
 // MongoDB connection
@@ -59,10 +57,9 @@ io.on('connection', (socket) => {
     socket.on('sendImage', (data) => {
         const partnerSocketId = engagedUsers[socket.id];
         if (partnerSocketId && users[partnerSocketId]) {
-            io.to(partnerSocketId).emit('receiveImage', {
-                from: users[socket.id].username,
-                image: data.image, // Base64-encoded image
-            });
+            io.to(partnerSocketId).emit('receiveImage', { from: users[socket.id].username, image: data.image });
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log(`❌ User disconnected: ${socket.id}`);
